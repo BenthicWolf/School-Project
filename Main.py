fb094de0d67b6b1926d844c408129d18a08aebfa
BenthicWolf <jarrett.osselton@gmail.com>
2021-09-10 17:20:41 +0100

    Added label

--- Main.py
+++ Main.py
@@ -1,2 +1,33 @@
 
-import _tkinter
+import tkinter
+from tkinter import *
+
+
+def Pressed():
+    pass
+
+
+def createwindow():
+    window = tkinter.Tk()
+    window.geometry("400x400")
+    window.title("Project")
+
+    label = tkinter.Label(
+        text="Hello",
+        fg="white",
+        bg="navy blue",
+        width=200,
+        height=200
+    )
+    button1 = tkinter.Button(
+        text="Press",
+        width=10,
+        height=10,
+        bg="grey"
+    )
+
+    label.pack()
+    window.mainloop()
+
+
+createwindow()
