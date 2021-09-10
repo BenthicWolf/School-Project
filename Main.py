76414fd3d28db4eeca589040ba5e46c6f5fc31eb
BenthicWolf <jarrett.osselton@gmail.com>
2021-09-10 17:49:42 +0100

    Added entry and scuffed button

--- Main.py
+++ Main.py
@@ -7,27 +7,33 @@ def Pressed():
     pass
 
 
+label1 = tkinter.Label(
+    text="Hello",
+    fg="white",
+    bg="navy blue",
+    width=200,
+    height=200
+)
+
+
+def createntry():
+    label2 = tkinter.Label(text="Enter your text", height=5)
+    entry = tkinter.Entry(width=50)
+    button1 = tkinter.Button(label2, text="Input Text", command=Pressed)
+
+    label2.pack()
+    button1.pack()
+    entry.pack()
+
+
 def createwindow():
     window = tkinter.Tk()
     window.geometry("400x400")
     window.title("Project")
-
-    label = tkinter.Label(
-        text="Hello",
-        fg="white",
-        bg="navy blue",
-        width=200,
-        height=200
-    )
-    button1 = tkinter.Button(
-        text="Press",
-        width=10,
-        height=10,
-        bg="grey"
-    )
-
-    label.pack()
-    window.mainloop()
+    label1.pack()
 
 
-createwindow()
+createntry()
+# createwindow()
+
+label1.mainloop()
