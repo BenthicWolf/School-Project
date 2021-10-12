feb8b8a211555053a1f7786786a3ddbb18cf8dc8
BenthicWolf <90395524+BenthicWolf@users.noreply.github.com>
2021-10-12 13:55:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from functions import dealer_turn, userauth, Deal_Cards, Choice, convert, is_over
+import sys
+from functions import dealer_turn, userauth, Deal_Cards, Choice, convert, is_over, settle
 
 
 def main():
@@ -19,15 +20,19 @@ The dealer's faceup card is the {convert(dealer.Cards[0])}""")
     while not Choice(player):
         if is_over(player) == True:
             print("You're over!")
+            sys.exit()
         else:
             print("You're not over!")
 
     dealer_turn(dealer)
-
+    
+    settle(player, dealer)
 
 class Person:
     def __init__(self):
         self.Cards = []
+        self.bet = 0
+        self.balance = 100
 
 
 print("\n\nYou are playing: Blackjack (21)\n")
