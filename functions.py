b4b6e38621aa68699ff62854fcdfc5e0903a02c5
BenthicWolf <90395524+BenthicWolf@users.noreply.github.com>
2021-10-14 13:55:02 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -8,7 +8,7 @@ def userauth(player):
 
     Valid_Users = open("Users.txt", "r")
 
-    if f"{Uname} : {Upass}" in Valid_Users.read():
+    if f"{Uname} : {Upass}\n" in Valid_Users.read():
         print("\nSuccessfuly authorised user account\n\n")
         Valid_Users.close()
         player.name = Uname
@@ -23,21 +23,12 @@ def Deal_Cards(player):
     player.Cards.append(randint(1, 52))
     player.Cards.append(randint(1, 52))
 
-
 def Choice(player):
-    print("\nDo you want to place a bet, hit, or stand?")
+    
+    print("\nDo you want to hit or stand?")
     decision = str(input("Please enter one of the options: ")).lower()
 
-    if decision == "bet":
-        bet = int(input(f"How much would you like to bet, your current balance is {player.balance}: "))
-        
-        if bet > player.balance:
-            print("That is not a valid bet")
-        else:
-            player.balance -= bet
-            print(f"Bet of {bet} added, an equal amount has been removed from your balance")
-
-    elif decision == "hit":
+    if decision == "hit":
         Get_Card(player)
         print(f"\nYour new card is: {convert(player.Cards[-1])}")
 
@@ -61,12 +52,14 @@ def dealer_turn(dealer):
 
         for card in dealer.Cards:
             card -= ((card-1)//13)*13
-            total += card + 1
+            total += card
     
         print(f"The dealers current total is: {total}")
 
         if total > 21:
             print("The dealer is bust")
+            return False
+            
 
         elif total >= 17:
             break
@@ -75,20 +68,26 @@ def dealer_turn(dealer):
             Get_Card(dealer)
             print(f"\nThe dealer takes a card, their new card is:   {convert(dealer.Cards[-1])}")
             print()
-
-        
-
-
-
+    
+    return True
+    
 def settle(player, dealer):
     ptotal = 0
     dtotal = 0
 
     for card in player.Cards:
-        ptotal += int(card - ((card-1)//13)*13)
+        card -= ((card-1)//13)*13
+        if card > 10:
+            card = 10
+        ptotal += card
 
-    for card in player.Cards:
-        dtotal += int(card - ((card-1)//13)*13)
+
+    for card in dealer.Cards:
+        card -= ((card-1)//13)*13
+        if card > 10:
+            card = 10
+        dtotal += card
+
 
     if ptotal > dtotal:
         print("Congratulations, you win!")
@@ -97,12 +96,22 @@ def settle(player, dealer):
         score = File.read().split(f"{player.name} : ")[1].split('\n')[0]
         File.close() 
 
-        
+        player.balance += (player.bet)*2
+
+        if score < player.balance:
+            print(f"Your bet has been doubled! Your current balance is : {player.balance}, this beats highest score for this user, which is: {score}! Keep playing if you want to further this record!")
+
+        elif score >= player.balance:
+            print(f"Your bet has been doubled! Your current balance is : {player.balance}, however the highest score for this user is: {score}! Keep playing to try to beat this score!")
 
     elif ptotal == dtotal:
-        pass
+        player.balance += player.bet
+        print(f"""You have the same total as the dealer, so you draw. Your bet has been returned,
+        your current balance is:{player.balance}""")
+
     else:
-        pass
+        print("You lose!")
+        print(f"Your balance hasn't been returned, your current balance is: {player.balance}")
 
 def convert(card):
     card -= 1
@@ -118,6 +127,12 @@ def convert(card):
 
     if card == 1:
         card = "Ace"
+    if card == 11:
+        card = "Jack"
+    if card == 12:
+        card = "Queen"
+    if card == 13:
+        card = "King"
 
     return str(card)+" of "+suit
 
@@ -126,7 +141,10 @@ def is_over(player):
     total = 0
 
     for card in player.Cards:
-        total += int(card - ((card-1)//13)*13)
+        card -= ((card-1)//13)*13
+        if card > 10:
+            card = 10
+        total += card
 
     print(f"Your current total is: {total}")
 
